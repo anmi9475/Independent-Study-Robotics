351e2b76c7fbdfce0ec042398409001d9f5ee689
JensenLav <112203468+JensenLav@users.noreply.github.com>
2023-10-26 09:02:42 -0600

    Update RunBlockTest.py Took the code that attempted to solve the block problem out

--- Full-System/RunBlockTest.py
+++ Full-System/RunBlockTest.py
@@ -65,97 +65,82 @@ try:
     
     sleepRate = 0.75
     def projectToWorldCoords(gripperFrameCoords):
-        # given a goal position in gripper coords returns the displacements from the current pose in world coords
-        xB,yB,zB = gripperFrameCoords
-        # subtract 0.165 from block position in gripper frame to account for gripper length
-        zB -= 0.155
-        currentPose = ur.getPose() #SE3 Object
-        # print(f"Current Pose:\n{currentPose*1000}")
-        R = currentPose.R 
-        pX,pY,pZ = tuple(currentPose.t)
-        # xB,yB,zB here is the block position in the gripper frame which is aligned with the optoforce frame
-        P_goal = np.matmul(R,np.array([xB,yB,zB]).T)  # relative position of the block in world coordinates
-        print(f"P_goal:\n{P_goal}")
-        dX,dY,dZ = tuple(P_goal) # quantities and directions the the gripper frame should be incremented to be centered at the block 
-        return dX,dY,dZ
-        
-    def moveToBlock(blockPos):
-        # would be better if this was block object
-        # :blockPos is coord in gripper frame
-        dX,dY,dZ = projectToWorldCoords(blockPos) # goalPose in world coordinates
-        homePose = ur.getPose()
-        dZ  += 7/1000 # up 7 mm to avoid hitting lower block
-        goal1 = copy.deepcopy(homePose)
-        goal1.t[2] += dZ
-        ur.moveL(goal1)
-        time.sleep(sleepRate)
-        goal2 = goal1
-        goal2.t[0] += dX
-        goal2.t[1] += dY
-        ur.moveL(goal2)
-        time.sleep(sleepRate)
-        
-    def moveBackFromBlock(homePose):    
-        currentPose = ur.getPose()
-        # Move up 3 mm to avoid raise block to prevent friction from toppling lower block
-        goal1 = copy.deepcopy(currentPose)
-        goal1.t[2] -= 3/1000
-        ur.moveL(goal1)
-        time.sleep(sleepRate)
-        currentPose = ur.getPose()
-        dX,dY,dZ = tuple(homePose.t - currentPose.t)
-        # Move in the XY Plane then Z Axis
-        goal2 = copy.deepcopy(currentPose)
-        goal2.t[0] += dX
-        goal2.t[1] += dY
-        ur.moveL(goal2)
-        time.sleep(sleepRate)
-        # Move in Z Axis back to home
-        goal3 = copy.deepcopy(goal2)
-        goal3.t[2] += dZ
-        ur.moveL(goal3)
-        time.sleep(sleepRate)
-
-    goalBlock = blocks[1]
-    blockLength = 0.02
-    releaseCoords = goalBlock.gripperFrameCoords + goalBlock.getWorldFrameVerticalInGripper(blockLength)
-    verticalDist = 0.02
-    gX,gY,gZ = tuple(goalBlock.urPose.t)
-    res = np.matmul(goalBlock.urPose.R,(sm.SE3.Trans([gX,gY,gZ+verticalDist]).t - goalBlock.urPose.t))
-    # print(f"res: {projectToWorldCoords(res)} ")
-    # ur.openGripper() # Open gripper
-    # ur.testRoutine()
+    # given a goal position in gripper coords returns the displacements from the current pose in world coords
+    xB,yB,zB = gripperFrameCoords
+    # subtract 0.165 from block position in gripper frame to account for gripper length
+    zB -= 0.155
+    currentPose = ur.getPose() #SE3 Object
+    # print(f"Current Pose:\n{currentPose*1000}")
+    R = currentPose.R 
+    pX,pY,pZ = tuple(currentPose.t)
+    # xB,yB,zB here is the block position in the gripper frame which is aligned with the optoforce frame
+    P_goal = np.matmul(R,np.array([xB,yB,zB]).T)  # relative position of the block in world coordinates
+    print(f"P_goal:\n{P_goal}")
+    dX,dY,dZ = tuple(P_goal) # quantities and directions the the gripper frame should be incremented to be centered at the block 
+    return dX,dY,dZ
+    
+def moveToBlock(blockPos):
+    # would be better if this was block object
+    # :blockPos is coord in gripper frame
+    dX,dY,dZ = projectToWorldCoords(blockPos) # goalPose in world coordinates
     homePose = ur.getPose()
+    dZ  += 7/1000 # up 7 mm to avoid hitting lower block
+    goal1 = copy.deepcopy(homePose)
+    goal1.t[2] += dZ
+    ur.moveL(goal1)
+    time.sleep(sleepRate)
+    goal2 = goal1
+    goal2.t[0] += dX
+    goal2.t[1] += dY
+    ur.moveL(goal2)
+    time.sleep(sleepRate)
+    
+def moveBackFromBlock(homePose):    
+    currentPose = ur.getPose()
+    # Move up 3 mm to avoid raise block to prevent friction from toppling lower block
+    goal1 = copy.deepcopy(currentPose)
+    goal1.t[2] += 3/1000
+    ur.moveL(goal1)
+    time.sleep(sleepRate)
+    currentPose = ur.getPose()
+    dX,dY,dZ = tuple(homePose.t - currentPose.t)
+    # Move in the XY Plane then Z Axis
+    goal2 = copy.deepcopy(currentPose)
+    goal2.t[0] += dX
+    goal2.t[1] += dY
+    ur.moveL(goal2)
+    time.sleep(sleepRate)
+    # Move in Z Axis back to home
+    goal3 = copy.deepcopy(goal2)
+    goal3.t[2] += dZ
+    ur.moveL(goal3)
+    time.sleep(sleepRate)
+    
+    
+    goalBlock = blocks[1]
+blockLength = 0.02
+releaseCoords = goalBlock.gripperFrameCoords + goalBlock.getWorldFrameVerticalInGripper(blockLength)
+verticalDist = 0.02
+gX,gY,gZ = tuple(goalBlock.urPose.t)
+res = np.matmul(goalBlock.urPose.R,(sm.SE3.Trans([gX,gY,gZ+verticalDist]).t - goalBlock.urPose.t))
+# print(f"res: {projectToWorldCoords(res)} ")
+# ur.openGripper() # Open gripper
+# ur.testRoutine()
+homePose = ur.getPose()
 
-    for step in steps:
-        # Grasp and Move Home Step
-        grabPos,releasePos = step
-        releasePos[0] = releasePos[0] - 0.03
-        print("This is the releasePos" , releasePos)
-        print("This is the GrabPos" , grabPos)
-        moveToBlock(grabPos) 
-        print("Done moving to block")
-        ur.closeGripper(9) 
-        time.sleep(sleepRate)
-        moveBackFromBlock(homePose)
-        moveToBlock(releasePos)
-        ur.openGripper()
-        moveBackFromBlock(homePose)
+for step in steps:
+    # Grasp and Move Home Step
+    grabPos,releasePos = step
+    moveToBlock(grabPos) 
+    print("Done moving to block")
+    ur.closeGripper(9) 
+    time.sleep(sleepRate)
+    moveBackFromBlock(homePose)
+    moveToBlock(releasePos)
+    ur.closeGripper(55)
+    moveBackFromBlock(homePose)
+    ur.openGripper()
         
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     gripperController.openGripper()
     gripperController.disconnect()
